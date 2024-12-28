56296c708866905045d030494546970f5a7d86c4
abinkrishna <abin48515@gmail.com>
2024-12-28 07:23:03 -0800

    save

--- lib/textTask.dart
+++ lib/textTask.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class TextTasks extends StatefulWidget {
@@ -8,6 +10,24 @@ class TextTasks extends StatefulWidget {
 }
 
 class _TextTasksState extends State<TextTasks> {
+  Timer? _timer;
+   var tme;
+String? date;
+// void frequentupdate(){
+//   _timer=Timer.periodic(Duration(seconds: 1), (timer) {
+//     settime();
+//   },);
+// }
+
+  void settime(){
+   setState(() {
+  // DateTime dateToday =new DateTime.now(); 
+  DateTime dateToday =new DateTime.now(); 
+   date = dateToday.toString().substring(0,10);
+  }
+  );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,27 +44,49 @@ class _TextTasksState extends State<TextTasks> {
       body: Container(
         height: double.infinity,
         width: double.infinity,
+        padding: EdgeInsets.all(5),
         child: Column(
           children: [
+            SizedBox(height: 15,),
             Container(
               height: 100,
               child: Expanded(
                   flex: 1,
                   child: TextField(
+                    
+                            cursorColor: Colors.green,
                     decoration: InputDecoration(
+                          labelText: "Title",
+                labelStyle: TextStyle(color: Colors.green,fontSize: 20),
+                                      focusedBorder: OutlineInputBorder(borderSide: BorderSide(width: 2,color: Colors.green)),
+
                         border: OutlineInputBorder(
                             borderSide: BorderSide(color: Colors.green))),
                   )),
             ),
             Expanded(
                 child: TextField(
-              // keyboardType: TextInputType.streetAddress,
-              // maxLength: 15,
-              maxLines: 20,
-              decoration: InputDecoration(
+              maxLines: 30,
+              cursorColor: Colors.green,
+              decoration: InputDecoration(alignLabelWithHint: true,
+                labelText: "Description",
+                labelStyle: TextStyle(color: Colors.green,fontSize: 20,),
+                focusedBorder: OutlineInputBorder(borderSide: BorderSide(width: 2,color: Colors.green)),
                   border: OutlineInputBorder(
+                    
                       borderSide: BorderSide(color: Colors.green))),
-            ))
+            )),
+            Container(
+              height: 80,
+              width: double.infinity,
+              padding: EdgeInsets.only(left: 15,right: 15),
+              child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(date.toString(),style: TextStyle(color: Colors.green)),
+                  Text("data",style: TextStyle(color: Colors.green),)
+                ],
+              ),
+            )
           ],
         ),
       ),
